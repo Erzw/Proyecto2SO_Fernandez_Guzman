bd416a5a694bbd0b2a5991e36e360f5a611af763
AlejandroGuzman44 <alejandro.guzman@correo.unimet.edu.ve>
2022-06-30 19:20:45 -0400

    Se agrego la clase de simulacion sony

---
+++ src/simulacionsony/SimulacionSony.java
@@ -0,0 +1,15 @@
+package simulacionsony;
+
+public class SimulacionSony {
+
+    public static boolean simulacionActiva = false;
+    public static Simulacion sim = new Simulacion();
+    public static GUI_Simulacion gui = new GUI_Simulacion();
+    
+    public static void main(String[] args) {
+        
+        gui.setVisible(true);
+        
+    }
+    
+}
